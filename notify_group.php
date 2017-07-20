cf7380878dc3d9e41fa469980b427d4d145939aa
naovanij <naovanij@gmail.com>
2017-07-20 16:50:22 +0700

    Update notify_group.php

--- notify_group.php
+++ notify_group.php
@@ -8,7 +8,7 @@ $stickerPkg = 2; //stickerPackageId
 $stickerId = 34; //stickerId
  
 $res = notify_message($str,$stickerPkg,$stickerId,$token);
-print_r($res);
+//print_r($res);
 
 function notify_message($message,$stickerPkg,$stickerId,$token){
      $queryData = array(
