f9235427dce1d53cd97b9d7172da7012a40e0a4a
naovanij <naovanij@gmail.com>
2017-07-20 16:57:09 +0700

    Update notify_group.php

--- notify_group.php
+++ notify_group.php
@@ -1,32 +1,23 @@
 define('LINE_API',"https://notify-api.line.me/api/notify");
+define('LINE_TOKEN','mgHiaNHqIMUjqA2s1rjMY8mXbjzc5bqv4u3T4dY3erB');
 
-$token = "mgHiaNHqIMUjqA2s1rjMY8mXbjzc5bqv4u3T4dY3erB"; //ใส่Token ที่copy เอาไว้
+function notify_message($message){
 
-$str = "Hello"; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
-
-$stickerPkg = 2; //stickerPackageId
-$stickerId = 34; //stickerId
- 
-$res = notify_message($str,$stickerPkg,$stickerId,$token);
+    $queryData = array('message' => $message);
+    $queryData = http_build_query($queryData,'','&');
+    $headerOptions = array(
+        'http'=>array(
+            'method'=>'POST',
+            'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
+            		  ."Authorization: Bearer ".LINE_TOKEN."\r\n"
+                      ."Content-Length: ".strlen($queryData)."\r\n",
+            'content' => $queryData
+        )
+    );
+    $context = stream_context_create($headerOptions);
+    $result = file_get_contents(LINE_API,FALSE,$context);
+    $res = json_decode($result);
+	return $res;
+}
+$res = notify_message('hello');
 var_dump($res);
-function notify_message($message,$stickerPkg,$stickerId,$token){
-     $queryData = array(
-      'message' => $message,
-      'stickerPackageId'=>$stickerPkg,
-      'stickerId'=>$stickerId
-     );
-     $queryData = http_build_query($queryData,'','&');
-     $headerOptions = array(
-         'http'=>array(
-             'method'=>'POST',
-             'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
-                 ."Authorization: Bearer ".$token."\r\n"
-                       ."Content-Length: ".strlen($queryData)."\r\n",
-             'content' => $queryData
-         ),
-     );
-     $context = stream_context_create($headerOptions);
-     $result = file_get_contents(LINE_API,FALSE,$context);
-     $res = json_decode($result);
-  return $res;
- }
